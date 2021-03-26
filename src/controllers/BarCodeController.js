1991c302499f8238e5a4ed6555cccdbc26de13b9
Reinaldo Almeida <reinaldo.almeida@live.com>
2021-03-26 19:28:22 -0300

    [FIX] Change JavascriptBarcodeReader to Quagga

--- src/controllers/BarCodeController.js
+++ src/controllers/BarCodeController.js
@@ -4,15 +4,18 @@ const reader = require("../model/barcodeReader.js");
 
 module.exports = {
     async show(req, res) {
-        const { image } = req.query;
+        const { image, barcodeType } = req.query;
         if (!image)
             return res.status(404).send({ error: "Image is not defined" });
+
+        let barcodeTypeList = ["ean_reader"];
+        if (barcodeType) barcodeTypeList.push(barcodeType);
 
         const fileDownloaded = await download.barcodeDownload(image);
         if (!fileDownloaded)
             return res.status(500).send({ error: "BarCode read failed" });
 
-        const code = reader.barcodeReader(fileDownloaded);
+        const code = reader.barcodeReader(fileDownloaded, barcodeTypeList);
         code.then((code) => {
             remove.barcodeRemove(fileDownloaded);
             return res.status(200).send({ barcode: code });
--- src/model/barcodeReader.js
+++ src/model/barcodeReader.js
@@ -1,20 +1,41 @@
-const javascriptBarcodeReader = require("javascript-barcode-reader");
-module.exports.barcodeReader = async (image) => {
-    return await javascriptBarcodeReader({
-        /* Image file Path || {data: Uint8ClampedArray, width, height} || HTML5 Canvas ImageData */
-        image: image,
-        barcode: "code-93",
-        // barcodeType: 'industrial',
-        options: {
-            // useAdaptiveThreshold: true
-            // singlePass: true
+const Quagga = require("quagga").default;
+/**
+ * 
+ * Barcode Decoders
+ * 
+ * 
+    code_128_reader (default)
+    ean_reader
+    ean_8_reader
+    code_39_reader
+    code_39_vin_reader
+    codabar_reader
+    upc_reader
+    upc_e_reader
+    i2of5_reader
+    2of5_reader
+    code_93_reader
+*/
+
+module.exports.barcodeReader = async (image, type) => {
+    return await Quagga.decodeSingle(
+        {
+            src: image,
+            numOfWorkers: 0, // Needs to be 0 when used within node
+            inputStream: {
+                size: 800, // restrict input-size to be 800px in width (long-side)
+            },
+            decoder: {
+                readers: type, // List of active readers
+            },
         },
-    })
-        .then((code) => {
-            return code;
-        })
-        .catch((err) => {
-            console.log(`[ERROR][500][javascriptBarcodeReader] => ${err}`);
-            return false;
-        });
+        (result) => {
+            if (result.codeResult) {
+                return result.codeResult.code;
+            } else {
+                console.log("[ERROR][500][barcodeReader]");
+                return false;
+            }
+        }
+    );
 };
